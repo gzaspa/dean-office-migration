f14fde06420b3269914c40c29170b7d6af29d375
gzaspa <gzaspa@ukr.net>
2017-07-22 11:37:39 +0300

    add Faculty entity

---
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/Faculty.java
@@ -0,0 +1,27 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import javax.persistence.Column;
+
+public class Faculty extends NameEntity {
+    @Column(name="abbr", nullable = false, unique = true, length = 20)
+    private String abbr;
+    @Column(name="active", nullable = false)
+    private boolean active;
+    //DEAN
+
+    public String getAbbr() {
+        return abbr;
+    }
+
+    public void setAbbr(String abbr) {
+        this.abbr = abbr;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
