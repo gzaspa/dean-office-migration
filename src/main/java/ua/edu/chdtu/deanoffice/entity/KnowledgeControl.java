dc71030d164409919c59e4c5b691dc0de157eedd
gzaspa <gzaspa@ukr.net>
2017-07-25 19:07:26 +0300

    add NameWithEngEntity and inherit KnowledgeControl entity from it

--- src/main/java/ua/edu/chdtu/deanoffice/entity/KnowledgeControl.java
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/KnowledgeControl.java
@@ -1,10 +1,12 @@
 package ua.edu.chdtu.deanoffice.entity;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Entity
 @Table(name="knowledge_control")
-public class KnowledgeControl extends NameEntity {
+public class KnowledgeControl extends NameWithEngEntity {
     @Column(name="has_grade", nullable = false)
     private boolean hasGrade;
 
---
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/NameWithEngEntity.java
@@ -0,0 +1,17 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import javax.persistence.*;
+
+@MappedSuperclass
+public class NameWithEngEntity extends NameEntity {
+    @Column(name="name_eng", nullable = false, unique = true, length = 100)
+    private String nameEng;
+
+    public String getNameEng() {
+        return nameEng;
+    }
+
+    public void setNameEng(String nameEng) {
+        this.nameEng = nameEng;
+    }
+}
