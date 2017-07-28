d781442d54a05fb391bf99bc356c66fc77b83bf3
gzaspa <gzaspa@ukr.net>
2017-07-28 16:37:43 +0300

    add CoursesForGroups entity

--- src/main/java/ua/edu/chdtu/deanoffice/entity/CoursesForGroups.java
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/CoursesForGroups.java
@@ -1,7 +1,52 @@
 package ua.edu.chdtu.deanoffice.entity;
 
-/**
- * Created by user on 28.07.2017.
- */
-public class CoursesForGroups {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name="courses_for_groups")
+public class CoursesForGroups extends BaseEntity {
+    @ManyToOne
+    private Course course;
+    @ManyToOne
+    private StudentGroup studentGroup;
+    @ManyToOne
+    private Teacher teacher;
+    @Column(name="exam_date", nullable = true)
+    private Date examDate;
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public StudentGroup getStudentGroup() {
+        return studentGroup;
+    }
+
+    public void setStudentGroup(StudentGroup studentGroup) {
+        this.studentGroup = studentGroup;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Date getExamDate() {
+        return examDate;
+    }
+
+    public void setExamDate(Date examDate) {
+        this.examDate = examDate;
+    }
 }
