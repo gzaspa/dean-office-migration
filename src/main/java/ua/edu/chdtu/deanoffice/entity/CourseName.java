2c5d3fec57cf8d3c69beb3c19a680a11d22fd5f1
gzaspa <gzaspa@ukr.net>
2017-07-28 11:48:22 +0300

    add CourseName entity

---
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/CourseName.java
@@ -0,0 +1,18 @@
+package ua.edu.chdtu.deanoffice.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class CourseName extends NameWithEngEntity {
+    @Column(name="abbreviation", nullable = true, length = 15)
+    private String abbreviation;
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+}
