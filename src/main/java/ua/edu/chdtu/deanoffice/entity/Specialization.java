a246efa3c8d9f37fb2f77ec261bc18b39875f96e
gzaspa <gzaspa@ukr.net>
2018-01-09 19:06:08 +0200

    fix long fields size in Specialization

--- src/main/java/ua/edu/chdtu/deanoffice/entity/Specialization.java
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/Specialization.java
@@ -6,10 +6,7 @@ import ua.edu.chdtu.deanoffice.entity.superclasses.NameWithEngAndActiveEntity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import java.math.BigDecimal;
 
 @Entity
