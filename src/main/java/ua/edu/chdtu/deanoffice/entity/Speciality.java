a79e379564031e8bd1ac716dad5e1c778f82da86
ValentynTulub <metryumora@gmail.com>
2018-06-01 18:34:57 +0300

    Added field of study code to speciality.

--- src/main/java/ua/edu/chdtu/deanoffice/entity/Speciality.java
+++ src/main/java/ua/edu/chdtu/deanoffice/entity/Speciality.java
@@ -13,6 +13,8 @@ import javax.persistence.Entity;
 public class Speciality extends NameWithEngAndActiveEntity {
     @Column(name = "code", nullable = false, unique = true, length = 20)
     private String code;
+    @Column(name = "field_of_study_code", length = 20)
+    private String fieldOfStudyCode;
     @Column(name = "field_of_study", length = 150)
     private String fieldOfStudy;
     @Column(name = "field_of_study_eng", length = 150)
